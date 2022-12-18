fdb29029df44d77f71ab8e4bd9c9d3f0b917ed41
Ranjan mallawarachchi <96380657+Ranjan2012@users.noreply.github.com>
2022-12-18 14:23:25 +1030

    Add files via upload

---
+++ signal_handler.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rmallawa <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/20 14:15:09 by rmallawa          #+#    #+#             */
+/*   Updated: 2022/10/03 14:27:46 by rmallawa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "minishell.h"
+
+/*
+** Handles interruption signals on a running process
+**
+** @param		signo		The signal number
+** @return		N/A
+*/
+
+void	proc_signal_handler(int signo)
+{
+	if (signo == SIGINT)
+	{
+		ft_putstr("\n");
+		signal(SIGINT, proc_signal_handler);
+	}
+}
+
+/*
+** Handles interruption signals on the program
+**
+** @param		signo		The signal number
+** @return		N/A
+*/
+
+void	signal_handler(int signo)
+{
+	if (signo == SIGINT)
+	{
+		ft_putstr("\n");
+		display_prompt_msg();
+		signal(SIGINT, signal_handler);
+	}
+}
